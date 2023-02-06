39dfb385e777704fd42ebaeb0fd7ecb370ffbc1c
Will Sciaroni <wsciaroni@protonmail.com>
2023-02-05 19:06:44 -0600

    Initial Commit

---
+++ sudoku-solver/sudoku-solver.cpp
@@ -0,0 +1,101 @@
+﻿// sudoku-solver.cpp : Defines the entry point for the application.
+//
+
+#include "sudoku-solver.h"
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+/**
+ * @brief Read the input string to get the input vector
+ * @param filename 
+ * @return 
+ * 
+ * SAMPLE input:
+ * [["5","3",".",".","7",".",".",".","."],
+ *  ["6",".",".","1","9","5",".",".","."],
+ *  [".","9","8",".",".",".",".","6","."],
+ *  ["8",".",".",".","6",".",".",".","3"],
+ *  ["4",".",".","8",".","3",".",".","1"],
+ *  ["7",".",".",".","2",".",".",".","6"],
+ *  [".","6",".",".",".",".","2","8","."],
+ *  [".",".",".","4","1","9",".",".","5"],
+ *  [".",".",".",".","8",".",".","7","9"]]
+*/
+std::vector<std::vector<char>> readFileToArr(const std::string& filename) {
+	std::ifstream source;
+	source.open(filename);
+
+	std::vector<std::vector<char>> board;
+	board.resize(9);
+	char c = '#';
+	source >> std::skipws >> c; // Read "["
+	for (int i = 0; i < 9; i++) {
+		board[i].resize(9);
+		source >> std::skipws >> c; // Read "["
+		for (int j = 0; j < 9; j++) {
+			source >> std::skipws >> c; // Read "\""
+			source >> std::skipws >> c; // Read "Character"
+			board[i][j] = c;
+			//std::cout << "Character " << c << std::endl;
+			source >> std::skipws >> c; // Read "\""
+			source >> std::skipws >> c; // Read "," and line ending "]"
+		}
+		source >> std::skipws >> c; // Read "," and last "]
+	}
+	source.close();
+	return board;
+}
+
+void printArr(const std::vector<std::vector<char>>& arr) {
+	int colCount = 0;
+	for (const auto& v : arr) {
+		int rowCount = 0;
+		colCount++;
+		for (const auto& c : v) {
+			std::cout << " " << c;
+			rowCount++;
+			if (rowCount % 3 == 0) {
+				std::cout << " ";
+			}
+		}
+		std::cout << std::endl;
+		if (colCount % 3 == 0) {
+			std::cout << std::endl;
+		}
+	}
+}
+int main(int argc, char** argv)
+{
+	if (argc < 2) { return -1; }
+	std::string inputFilename(argv[1]);
+	std::vector<std::vector<char>> board = readFileToArr(inputFilename);
+
+	std::cout << "Array read in as follows: " << std::endl << std::endl;
+	printArr(board);
+
+	std::cout << std::endl << "Solving ..." << std::endl << std::endl;
+	Solution s;
+	auto startTime = std::chrono::high_resolution_clock::now();
+	s.solveSudoku(board);
+	auto stopTime = std::chrono::high_resolution_clock::now();
+
+	// Subtract stop and start timepoints and
+	// cast it to required unit. Predefined units
+	// are nanoseconds, microseconds, milliseconds,
+	// seconds, minutes, hours. Use duration_cast()
+	// function.
+	auto durationMicro = std::chrono::duration_cast<std::chrono::microseconds>(stopTime - startTime);
+	auto durationSec = std::chrono::duration_cast<std::chrono::seconds>(stopTime - startTime);
+
+	// To get the value of duration use the count()
+	// member function on the duration object
+	std::cout << "Completed in " << durationMicro.count() << " microseconds" << std::endl;
+	std::cout << "Completed in " << durationSec.count() << " seconds" << std::endl;
+
+	std::cout << "Output array: " << std::endl << std::endl;
+	printArr(board);
+	return 0;
+}
---
+++ sudoku-solver/sudoku-solver.h
@@ -0,0 +1,243 @@
+﻿// sudoku-solver.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <bitset>
+#include <vector>
+#include <cassert>
+#include <algorithm>
+
+class Cell {
+public:
+   // The true value of the cell
+   uint8_t value = 0;
+   bool valueIsSet = false;
+
+   // Number of possible unconstrained values for the cell
+   uint8_t numberOfPossibilities = 9;
+
+   std::bitset<10> constraints = 0b0000000000; // 0b1111111111;
+};
+
+class Solution {
+   bool loggingEnabled = false;
+
+   void printVectorState(std::vector<std::vector<Cell>>& vect) {
+      if (!loggingEnabled) return;
+      std::cout << "[" << std::endl;
+      for (int i = 0; i < 9; i++) {
+         std::cout << "[";
+         for (int j = 0; j < 9; j++) {
+            std::cout << "\"" << (char)(vect[i][j].value + '0') << "\"";
+         }
+         std::cout << "]," << std::endl;
+      }
+      std::cout << "]" << std::endl;
+   }
+
+   void initialize() {
+      cells.resize(9);
+      for (auto& arr : cells) {
+         arr.resize(9);
+      }
+
+      for (int i = 0; i < 9; i++) {
+         for (int j = 0; j < 9; j++) {
+            cells[i][i].value = 0;
+         }
+      }
+
+      if (loggingEnabled) {
+         std::cout << "Initialzied all cells" << std::endl;
+      }
+   }
+
+   bool setValue(int i, int j, int value) {
+      if (loggingEnabled) {
+         std::cout << "Setting value at: [" << i << "," << j << "]: " << (char)(value + '0') << std::endl;
+      }
+      Cell& c = cells[i][j];
+
+      // We've already determined this value and it matches
+      if (c.valueIsSet && c.value == value) {
+         if (loggingEnabled) {
+            std::cout << "Value already set" << std::endl;
+         }
+         return true;
+      }
+
+      // If we've already deterimined that this cell can't be this value, return false
+      if (c.constraints[value]) {
+         if (loggingEnabled) {
+            std::cout << "Cannot set, this violates the constraints from earlier..." << std::endl;
+         }
+         return false;
+      }
+
+      if (loggingEnabled) {
+         std::cout << "Setting this value." << std::endl;
+      }
+
+      c.constraints = std::bitset<10>(0b1111111111); // 10 ones
+      c.constraints.reset(value);
+      c.numberOfPossibilities = 1;
+      c.value = value;
+      c.valueIsSet = true;
+
+      for (int k = 0; k < 9; k++) {
+         // Apply constraints to the row
+         if (i != k) {
+            if (!updateConstraints(k, j, value)) {
+               if (loggingEnabled) {
+                  std::cout << "Unable to apply the constraints on the row" << std::endl;
+               }
+               return false; // Unable to update the constraints on the row
+            }
+         }
+
+         // Apply constraints to the column
+         if (j != k) {
+            if (!updateConstraints(i, k, value)) {
+               if (loggingEnabled) {
+                  std::cout << "Unable to apply the constraints on the column" << std::endl;
+               }
+               return false; // Unable to update the constraints on the column
+            }
+         }
+
+         // To the 3x3 square
+         // Index of corners of the square.
+         int ix = (i / 3) * 3 + k / 3;
+         int jx = (j / 3) * 3 + k % 3;
+         if (ix != i && jx != j) {
+            if (!updateConstraints(ix, jx, value)) {
+               if (loggingEnabled) {
+                  std::cout << "Unable to apply the constraints on the square" << std::endl;
+               }
+               return false; // Unable to update the constraints on the 3x3 square.
+            }
+         }
+      }
+
+      return true;
+   }
+
+   bool updateConstraints(int i, int j, int excludedValue) {
+      if (loggingEnabled) {
+         std::cout << "Attempting to exclude the value " << (char)(excludedValue + '0') << " at [" << i << "," << j << "]" << std::endl;
+      }
+      Cell& c = cells[i][j];
+
+      if (c.constraints[excludedValue]) {
+         if (loggingEnabled) {
+            std::cout << "Constraints already set" << std::endl;
+         }
+         return true;
+      }
+      if (c.valueIsSet && c.value == excludedValue) {
+         if (loggingEnabled) {
+            std::cout << "Can't constrain field. Value already set" << std::endl;
+         }
+         return false; // We were wrong in our attempt
+      }
+
+      // If the value could be valid, AND the constraints don't have this excluded, let's remove it from the constraints
+      c.constraints.set(excludedValue);
+      c.numberOfPossibilities--;
+
+      if (c.numberOfPossibilities > 1) return true; // If we haven't reached the last number of possibilities
+
+      // We've reached the last posibility for the individual value
+      for (int v = 1; v <= 9; v++) {
+         if (!c.constraints[v]) {
+            return setValue(i, j, v);
+         }
+      }
+
+      // This should never happen
+      // throw std::logic_error("Somehow the Cell has 1 possibility remaining, but none available in the set function");
+      assert(false);
+   }
+
+   // Keep a list of empty cells at the beginning to back track
+   std::vector<std::pair<int, int>> bt;
+
+   bool findValuesForEmptyCells() {
+      bt.clear();
+
+      for (int i = 0; i < 9; i++) {
+         for (int j = 0; j < 9; j++) {
+            if (!cells[i][j].valueIsSet) {
+               bt.emplace_back(i, j);
+            }
+         }
+      }
+
+      // Sort the list by the number of possibilites remaining in each cell
+      std::sort(bt.begin(), bt.end(), [this](const std::pair<int, int>& a, const std::pair<int, int>& b) {
+         return cells[a.first][a.second].numberOfPossibilities < cells[b.first][b.second].numberOfPossibilities;
+         });
+      return backtrack(0);
+   }
+
+   bool backtrack(int k) {
+      if (k >= bt.size()) return true;
+
+      auto i = bt[k].first;
+      auto j = bt[k].second;
+
+      // Fast path
+      if (cells[i][j].valueIsSet) return backtrack(k + 1);
+
+      auto constraints = cells[i][j].constraints;
+
+      auto snapshot = cells; // Create a copy of the array as a backup
+
+      for (int v = 1; v <= 9; v++) {
+         if (!constraints[v]) {
+            if (setValue(i, j, v)) {
+               if (backtrack(k + 1)) {
+                  return true;
+               }
+            }
+            cells = snapshot;
+         }
+      }
+      return false;
+   }
+
+   std::vector<std::vector<Cell>> cells; // 9x9 vector of cell
+
+public:
+   void solveSudoku(std::vector<std::vector<char>>& board) {
+      initialize();
+
+      for (int i = 0; i < 9; i++) {
+         for (int j = 0; j < 9; j++) {
+            if (board[i][j] != '.') {
+               if (!setValue(i, j, board[i][j] - '0'))
+               {
+                  if (loggingEnabled) {
+                     std::cout << "Unable to initialize, Either invalid, or unsolvable" << std::endl;
+                  }
+                  return; // unsolvable. Can't set the values that we were given
+               }
+            }
+         }
+      }
+
+      if (!findValuesForEmptyCells()) return; // unsolvable.
+
+      for (int i = 0; i < 9; i++) {
+         for (int j = 0; j < 9; j++) {
+            if (cells[i][j].valueIsSet) {
+               board[i][j] = cells[i][j].value + '0';
+            }
+         }
+      }
+
+      printVectorState(cells);
+   }
+};
